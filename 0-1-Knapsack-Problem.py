28cc8b38b9fcfcc8ecc68c33109eb7f4768a8d05
Mehta <63797687+dipalimehta@users.noreply.github.com>
2023-12-15 21:26:39 +0530

    Create 0-1-Knapsack-Problem.py

---
+++ 0-1-Knapsack-Problem.py
@@ -0,0 +1,29 @@
+# Dynamic Programming - 0/1 Knapsack problem
+
+# Table initialization
+t  = [[-1 for _ in range(C+1)] for _ in range(n+1)]
+
+def knapsack(wt,v,C,n,t):
+    # Base Condn
+    if n==0 or C==0:
+        return 0
+    # Memozise
+    if t[n][C] != -1:
+        return t[n][C]
+    # Choice diagram
+    if wt[n-1]>C:
+        t[n][C] = knapsack(wt,v,C,n-1)
+    else:
+        t[n][C] = max(
+                             v[n-1] + knapsack(wt,v,C-wt[n-1],n-1),
+                             knapsack(wt,v,C,n-1) 
+                            )  
+    return t[n][C]
+
+# inputs
+wt = [5, 4, 6, 3]
+v = [10, 40, 30, 50]
+n = len(wt)
+C = 10
+
+knapsack(wt,v,C,n)
